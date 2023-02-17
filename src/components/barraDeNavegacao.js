5524be857dda9e368159580f3adbbabd33dad1f3
elmojuh <junior.fool.skull@gmail.com>
2023-02-16 21:07:53 -0300

    subindo alteraçoes

--- src/components/barraDeNavegacao.js
+++ src/components/barraDeNavegacao.js
@@ -5,33 +5,42 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function BarraDeNavegacao() {
+    const formatacaoIcones={
+        paddingRight: '10px',
+        maxWidth: '30px',
+    }
+    const linkMenu={
+        borderBottom: '2px solid',
+        borderRadius: '5px',
+    }
+    const linkProduto={
+        borderBottom: '2px solid',
+        borderRadius: '5px',
+        borderColor: '#a3a3a3'
+    }
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
 
-                <Navbar.Brand href="/home"><Image src='/images/logo/logo_01.png' height="30px" style={{ paddingRight: "10px" }} />Art Acril</Navbar.Brand>
+                <Navbar.Brand href="/"><Image src='/images/logo/logo_01.png' height="30px" style={{ paddingRight: "10px" }} />Art Acril</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/home">Home</Nav.Link>
-                        <Nav.Link href="https://shopee.com.br/artacril" target='_blank'>Loja na Shopee</Nav.Link>
-                        <Nav.Link href="https://drive.google.com/file/d/1AeQ3uyqqTskXBLwkAiUjro56C3RCeN1u/view?usp=sharing" target='_blank'>Catálogo 2023</Nav.Link>
-                        <NavDropdown title="Produtos" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Busto</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Base
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Kits<Image src='/images/produtos/Expositor_Busto_MAN_Preto_de_Lado.jpeg' width={"30px"} /></NavDropdown.Item>
+                        <Nav.Link style={linkMenu} href="/">Home</Nav.Link>
+                        <Nav.Link style={linkMenu} href="https://shopee.com.br/artacril" target='_blank'><img style={formatacaoIcones} src='https://img.icons8.com/color/480/shopee.png'/>Loja na Shopee</Nav.Link>
+                        <Nav.Link style={linkMenu} href="https://drive.google.com/file/d/1AeQ3uyqqTskXBLwkAiUjro56C3RCeN1u/view?usp=sharing" target='_blank'>Catálogo 2023</Nav.Link>
+                        <NavDropdown style={linkProduto} title="Produtos" >
+                            <NavDropdown.Item  href="#action/3.1">Bustos</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">
-                                Outros
+                                Sessão Indisponível
                             </NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="/sobre-nos">Sobre Nós</Nav.Link>
-                        <Nav.Link eventKey={2} href="http://wa.me/5521980304952/" target='_blank'>
-                            Faça um Orçamento
+                        <Nav.Link style={linkMenu} href="/sobre-nos">Sobre Nós</Nav.Link>
+                        <Nav.Link style={linkMenu} eventKey={2} href="http://wa.me/5521980304952/" target='_blank'>
+                        <img style={formatacaoIcones} src='https://imagepng.org/wp-content/uploads/2017/08/whatsapp-icone-4.png'/>Faça um Orçamento 
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
---
+++ src/components/cardSobreNos.js
@@ -0,0 +1,35 @@
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+function CardSobreNos() {
+    const fundoCard = {
+        backgroundColor: 'grey',
+        color: '#ffffff',
+    }
+    const fundoFoto = {
+        backgroundColor: '#193366',
+        padding: '10px'
+    }
+    return (
+        <Row xs={1} md={2} className="g-4">
+            {Array.from({ length: 1 }).map((_, idx) => (
+                <Col>
+                    <Card style={fundoCard}>
+                        <Card.Img variant="top" src="/images/logo/logo_02.png" style={fundoFoto}/>
+                        <Card.Body>
+                            <Card.Text >
+                                A Art Acril se orgulha de sua longa tragetória em sua pequena fábrica de expositores em acrílico do Seu Geovanne.
+                                Que ao longo de 30 anos cria modelos artezanais com o maior gral de sofisticação e requinte.
+                                É reconhecido até hoje como referência em beleza e resistencia de materiais em acrílico de primeira linha
+                                pelos joalheiros e logistas do Rio de Janeiro.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ))}
+        </Row>
+    );
+}
+
+export default CardSobreNos;
